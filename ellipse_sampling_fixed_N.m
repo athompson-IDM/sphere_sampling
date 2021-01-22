2029ec2c4a66b91fa6a3b112157c378991cedbae
Ace Thompson <athompson@idmod.org>
2021-01-22 10:48:13 -0800

    Added comments, and control of pause mode.

--- ellipse_sampling_fixed_N.m
+++ ellipse_sampling_fixed_N.m
@@ -1,6 +1,14 @@
+% Set this to 0 if you just want to generate figure 2, and you'll also
+% get the last trial in figure 1.
+%
+% Leave it 1 to step one trial at a time through co-plotting 
+% the sampled ellipse with the normal distribution of equivalent
+% sigma.
+pause_mode = 0
+
 num_samples = 1e5;
 ruler_dimension = 1;
-num_trials = 100;
+num_trials = 5;
 n_dist_bins=100;
 N = 100;
 % radius of ball of volume 1.
@@ -15,8 +23,9 @@ for k = 1:num_trials
     % First calculate ellipse axis lengths.
     % It will be aligned with the cartesian axes.
     % Just need a1 * a2 * ... * aN = R, where R is the equivalent
-    % radius of a volume one sphere in dimension N.
+    % radius of a volume one sphere in dimension N.    
     ellipse_a = zeros(1,N);
+    % So we'll chop R up into the pieces a_i.
     R_remaining = R;
     for i = 1:N
         if i == N
@@ -39,9 +48,12 @@ for k = 1:num_trials
     bin_width = bin_ellipse(2) - bin_ellipse(1)
     figure(1)
     hold off
-    %y_ellipse_normalized = y_ellipse / sum(y_ellipse);
-    y_ellipse_normalized = y_ellipse / (num_samples*bin_width)
-    %y_ellipse_normalized = y_ellipse_normalized * R * 2 / sqrt(ellipse_a(ruler_dimension));
+    
+    % Something isn't quite right with this normalization, but
+    % the same thing is wrong with the normalization of the coplotted
+    % function below so it's ok for now.
+    
+    y_ellipse_normalized = y_ellipse / (num_samples*bin_width)    
     plot(bin_ellipse,y_ellipse_normalized, 'o')
     ellipse_sigmas(k) = std(X_ellipse(:,ruler_dimension));
     ellipse_conds(k)  = cond(ellipse_cov);
@@ -49,16 +61,20 @@ for k = 1:num_trials
     % normal dist w/ equiv sigma
     nd = ellipse_sigmas(k) * randn(num_samples,1);
     [y_nd,bin_nd] = hist(nd,n_dist_bins);
-    bin_width = bin_nd(2) - bin_nd(1)
-    %y_nd_normalized = y_nd / sum(y_nd);
+    bin_width = bin_nd(2) - bin_nd(1)    
+    
+    % This normalization has the same problem as the other one described
+    % above, but they are wrong in the same way so it's ok for now.
     y_nd_normalized = y_nd / (num_samples*bin_width);
     hold on
     plot(bin_nd, y_nd_normalized, '-')
     axis([-1,1,0,10])
     legend('sampled ellipse','normal dist w/ same sigma')
     title(sprintf('Distribution of x_%d in uniformly sampled random ellipse of volume 1, N=%d',ruler_dimension,N),'fontsize',8)
-    pause
-    
+        
+    if pause_mode == 1
+        pause
+    end
 end
 
 
